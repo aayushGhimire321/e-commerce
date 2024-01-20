7d11af2d01bbf23284a2c8664061b90b019c1ed0
Aayush Ghimire <112367570+aayushghimire123@users.noreply.github.com>
2024-01-20 18:44:45 +0545

    work going on

--- backend/ecommerce/ecommerce/src/main/java/com/in/ecommerce/service/CategoryService.java
+++ backend/ecommerce/ecommerce/src/main/java/com/in/ecommerce/service/CategoryService.java
@@ -1,8 +1,17 @@
 package com.in.ecommerce.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.in.ecommerce.model.Category;
+import com.in.ecommerce.repository.CategoryRepo;
 
 @Service
 public class CategoryService {
     
+    @Autowired
+    CategoryRepo CategoryRepo;
+
+    public void createCategory(Category category){
+        CategoryRepo.save(category);
+    }
 }
